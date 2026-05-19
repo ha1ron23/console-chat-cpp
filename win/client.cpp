df4db5ef7b98b9b665cf707ca3bb74cf103807ba
hairon <h4304577@gmail.com>
2026-05-19 15:11:53 +0400

    Update client.cpp

--- win/client.cpp
+++ win/client.cpp
@@ -2,9 +2,11 @@
 #include <string>
 #include <cstring>
 #include <atomic>
+#include <thread>
+#include <chrono>
+#include <conio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <conio.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -13,107 +15,92 @@
 
 std::atomic<bool> running(true);
 SOCKET sock = INVALID_SOCKET;
+std::string input_buffer;
 
-bool receive_once() {
+void add_message(const std::string& msg) {
+    std::cout << "\r\033[K" << msg << std::flush;
+    std::cout << "\r> " << input_buffer << std::flush;
+}
+
+void receive_messages() {
     char buffer[BUFFER_SIZE];
-    u_long mode = 1;
-    ioctlsocket(sock, FIONBIO, &mode);
-    int bytes = recv(sock, buffer, BUFFER_SIZE - 1, 0);
-    mode = 0;
-    ioctlsocket(sock, FIONBIO, &mode);
-    if (bytes > 0) {
+    while (running) {
+        int bytes = recv(sock, buffer, BUFFER_SIZE - 1, 0);
+        if (bytes <= 0) {
+            add_message("\n[System] Server disconnected. Exiting...\n");
+            running = false;
+            break;
+        }
         buffer[bytes] = '\0';
         std::string msg(buffer);
         if (msg == "SERVER_SHUTDOWN\n") {
-            std::cout << "Server is shutting down. Exiting..." << std::endl;
-            return false;
+            add_message("\n[System] Server shutting down. Exiting...\n");
+            running = false;
+            break;
         }
-        std::cout << msg << std::flush;
-        return true;
-    } else if (bytes == 0) {
-        std::cout << "Server disconnected. Exiting..." << std::endl;
-        return false;
+        add_message(msg);
     }
-    return true;
-}
-
-void send_message(const std::string& msg) {
-    std::string out = msg + "\n";
-    send(sock, out.c_str(), out.size(), 0);
+    closesocket(sock);
+    WSACleanup();
+    exit(0);
 }
 
 int main() {
     WSADATA wsaData;
-    struct sockaddr_in server_addr;
-    std::string username;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+    std::string username;
     std::cout << "Enter your username: ";
     std::getline(std::cin, username);
 
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        std::cerr << "WSAStartup failed" << std::endl;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed\n";
         return 1;
     }
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        std::cerr << "Socket creation failed" << std::endl;
-        WSACleanup();
-        return 1;
-    }
-
+    struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
     if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        std::cerr << "Connect failed. Is the server running?" << std::endl;
-        closesocket(sock);
-        WSACleanup();
+        std::cerr << "Connect failed. Is server running?\n";
         return 1;
     }
-
     send(sock, username.c_str(), username.size(), 0);
 
-    std::cout << "Connected to chat. Type /quit to exit." << std::endl;
+    std::cout << "Connected. Type /quit to exit.\n";
+    std::cout << "> " << std::flush;
 
-    std::string input;
-    bool input_mode = false;
+    std::thread receiver(receive_messages);
+    receiver.detach();
 
-    // Main loop
     while (running) {
-        if (!receive_once()) {
-            running = false;
-            break;
-        }
-
         if (_kbhit()) {
             char ch = _getch();
-            if (ch == '\r') {
-                if (!input.empty()) {
-                    if (input == "/quit") break;
-                    send_message(input);
-                    input.clear();
+            if (ch == '\r') {  // Enter
+                if (!input_buffer.empty()) {
+                    std::string out = input_buffer + "\n";
+                    send(sock, out.c_str(), out.size(), 0);
+                    if (input_buffer == "/quit") break;
+                    input_buffer.clear();
+                    std::cout << "\r> " << std::flush;
                 }
-                std::cout << std::endl;
-                input_mode = false;
-            } else if (ch == '\b') {
-                if (!input.empty()) {
-                    input.pop_back();
-                    std::cout << "\b \b";
+            } else if (ch == '\b') {  // Backspace
+                if (!input_buffer.empty()) {
+                    input_buffer.pop_back();
+                    std::cout << "\b \b" << std::flush;
                 }
-            } else {
-                input.push_back(ch);
-                std::cout << ch;
-                input_mode = true;
+            } else if (ch >= 32 && ch <= 126) {
+                input_buffer.push_back(ch);
+                std::cout << ch << std::flush;
             }
-        } else if (input_mode) {
         }
-
-        Sleep(50);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
+    running = false;
     closesocket(sock);
     WSACleanup();
-    std::cout << "Chat ended: server exit." << std::endl;
+    std::cout << "\nGoodbye.\n";
     return 0;
 }
