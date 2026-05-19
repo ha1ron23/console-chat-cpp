835d06ec723532d1c68c3cdd3c242f030a187e72
hairon <h4304577@gmail.com>
2026-05-20 01:21:55 +0400

    Update client.cpp
    commands support and language support(tested on english and russian) also updated formatting

--- win/client.cpp
+++ win/client.cpp
@@ -4,22 +4,26 @@
 #include <atomic>
 #include <thread>
 #include <chrono>
-#include <conio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
 #define PORT 9034
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 4096
 
 std::atomic<bool> running(true);
 SOCKET sock = INVALID_SOCKET;
-std::string input_buffer;
+std::string my_username;
+std::string my_role = "Member";
+
+void set_utf8() {
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+}
 
 void add_message(const std::string& msg) {
-    std::cout << "\r\033[K" << msg << std::flush;
-    std::cout << "\r> " << input_buffer << std::flush;
+    std::cout << msg << std::flush;
 }
 
 void receive_messages() {
@@ -34,9 +38,49 @@ void receive_messages() {
         buffer[bytes] = '\0';
         std::string msg(buffer);
         if (msg == "SERVER_SHUTDOWN\n") {
-            add_message("\n[System] Server shutting down. Exiting...\n");
+            add_message("\n[System] Server is shutting down. Exiting...\n");
             running = false;
             break;
+        }
+        if (msg.substr(0, 10) == "YOUR_ROLE ") {
+            std::string rest = msg.substr(10);
+            size_t sp = rest.find(' ');
+            if (sp != std::string::npos) {
+                my_role = rest.substr(0, sp);
+                std::string cmd_list = rest.substr(sp+1);
+                std::cout << "Your role: " << my_role << "\n";
+                if (!cmd_list.empty())
+                    std::cout << "Commands: " << cmd_list << "\n";
+            }
+            continue;
+        }
+        if (msg.substr(0, 13) == "ROLE_CHANGED ") {
+            std::string rest = msg.substr(13);
+            size_t sp = rest.find(' ');
+            if (sp != std::string::npos) {
+                my_role = rest.substr(0, sp);
+                add_message("Your role changed to " + my_role + "\n");
+            }
+            continue;
+        }
+        if (msg.find("Commands available:") != std::string::npos) continue;
+        size_t colon = msg.find(':');
+        if (colon != std::string::npos) {
+            std::string before = msg.substr(0, colon);
+            std::string after = msg.substr(colon + 1);
+            size_t bracket_open = before.find('[');
+            size_t bracket_close = before.find(']');
+            if (bracket_open != std::string::npos && bracket_close != std::string::npos) {
+                std::string role_part = before.substr(bracket_open, bracket_close - bracket_open + 1);
+                std::string name_part = before.substr(bracket_close + 1);
+                name_part.erase(0, name_part.find_first_not_of(' '));
+                name_part.erase(name_part.find_last_not_of(' ') + 1);
+                if (name_part == my_username) {
+                    msg = role_part + " " + name_part + " (you):" + after;
+                } else {
+                    msg = before + ":" + after;
+                }
+            }
         }
         add_message(msg);
     }
@@ -46,16 +90,17 @@ void receive_messages() {
 }
 
 int main() {
+    set_utf8();
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    std::string username;
     std::cout << "Enter your username: ";
-    std::getline(std::cin, username);
+    std::getline(std::cin, my_username);
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == INVALID_SOCKET) {
         std::cerr << "Socket creation failed\n";
+        WSACleanup();
         return 1;
     }
     struct sockaddr_in server_addr;
@@ -64,38 +109,23 @@ int main() {
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         std::cerr << "Connect failed. Is server running?\n";
+        closesocket(sock);
+        WSACleanup();
         return 1;
     }
-    send(sock, username.c_str(), username.size(), 0);
+    send(sock, my_username.c_str(), my_username.size(), 0);
 
     std::cout << "Connected. Type /quit to exit.\n";
-    std::cout << "> " << std::flush;
 
     std::thread receiver(receive_messages);
     receiver.detach();
 
+    std::string input;
     while (running) {
-        if (_kbhit()) {
-            char ch = _getch();
-            if (ch == '\r') {  // Enter
-                if (!input_buffer.empty()) {
-                    std::string out = input_buffer + "\n";
-                    send(sock, out.c_str(), out.size(), 0);
-                    if (input_buffer == "/quit") break;
-                    input_buffer.clear();
-                    std::cout << "\r> " << std::flush;
-                }
-            } else if (ch == '\b') {  // Backspace
-                if (!input_buffer.empty()) {
-                    input_buffer.pop_back();
-                    std::cout << "\b \b" << std::flush;
-                }
-            } else if (ch >= 32 && ch <= 126) {
-                input_buffer.push_back(ch);
-                std::cout << ch << std::flush;
-            }
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        if (!std::getline(std::cin, input)) break;
+        if (input == "/quit") break;
+        std::string out = input + "\n";
+        send(sock, out.c_str(), out.size(), 0);
     }
 
     running = false;
